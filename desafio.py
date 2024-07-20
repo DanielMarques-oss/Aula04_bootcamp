f1ebe1d7498c4b3a4da0b3c7a7b13e411648af07
Danie Marques de Almeida <danielmarques.enfermagem@gmail.com>
2024-07-20 13:52:52 -0300

    Refatorando o código para utilizar as variáveis salario_valido e bonus_valido em  while

--- desafio.py
+++ desafio.py
@@ -3,11 +3,13 @@
 # informações corretas
 
 # Solicita ao usuário que digite seu nome
-nome_valido: bool = False
-salario_valido: bool = False
-bonus_valido: bool = False
+
 
 def cadastrar_usuario_com_salario_e_bonus():
+    nome_valido: bool = False
+    salario_valido: bool = False
+    bonus_valido: bool = False
+    
     while not nome_valido:
         try:
             nome: str = input("Digite seu nome: ")
@@ -25,25 +27,25 @@ def cadastrar_usuario_com_salario_e_bonus():
             print(e)
 
     # Solicita ao usuário que digite o valor do seu salário e converte para float
-    while True:
+    while not salario_valido:
         try:
             salario: float = float(input("Digite o valor do seu salário: "))
             if salario < 0:
                 print("Por favor, digite um valor positivo para o salário.")
             else:
-                break
+                salario_valido = True
         except ValueError:
             print("Entrada inválida para o salário. Por favor, digite um número.")
 
 
     # Solicita ao usuário que digite o valor do bônus recebido e converte para float
-    while True:
+    while not bonus_valido:
         try:
             bonus: float = float(input("Digite o valor do bônus recebido: "))
             if bonus < 0:
                 print("Por favor, digite um valor positivo para o bônus.")
             else:
-                break
+                bonus_valido = True
         except ValueError:
             print("Entrada inválida para o bônus. Por favor, digite um número.")
 
@@ -52,3 +54,5 @@ def cadastrar_usuario_com_salario_e_bonus():
 
     # Imprime as informações para o usuário
     print(f"{nome}, seu salário é R${salario:.2f} e seu bônus final é R${bonus_recebido:.2f}.")
+
+cadastrar_usuario_com_salario_e_bonus()
